7752c6ebe1be91a0ac30367068e76742178cd94d
lstndrms <lstndrms13@gmail.com>
2021-02-18 01:30:27 +0300

    1802

--- 123.c
+++ 123.c
@@ -15,14 +15,18 @@ void rand_d(double *a, int n) {
     }
 }
 
-void shellsort(double *a, int n) {
+void shellsort(double *a, int n, int c_cmp, int c_swap) {
     int j;
     for (int s = n / 2;s > 0;s /= 2) {
         for (int i = s; i < n;i++) {
             double tmp = a[i];
             for (j = i;(j >= s) && (fabs(a[j - s]) < fabs(tmp));j -= s) {
                 a[j] = a[j - s];
+                c_cmp++;
+                c_swap++;
             }
+            if (j != i)
+                c_swap++;
             a[j] = tmp;
         }
     }
@@ -57,20 +61,36 @@ void heapsort(double *a, int n) {
 }
 
 signed main(void) {
+    srand((int)time(0));
+    int test_case[4] = {10, 100, 1000, 10000};
+    for (int t = 0;t < 4;t++) {
+        int n = test_case[t];
+        double *a = (double*)malloc(n * sizeof(double)),
+               *b = (double*)malloc(n * sizeof(double));
+        rand_d(a, n);
+        for (int i = 0;i < n;i++) {
+            b[i] = a[i];
+        }
+        int cnt_swap_shell = 0, cnt_swap_heap = 0,
+            cnt_cmp_shell = 0, cnt_cmp_heap = 0;
+        shellsort(a, n, cnt_cmp_shell, cnt_swap_shell);
+        heapsort(b, n);//, cnt_cmp_heap, cnt_swap_heap);
+        printf("n = %d\n", n);
+        printf("SHELL:\ncompare times: %d, swaps: %d\n", cnt_cmp_shell, cnt_swap_shell);
+        printf("HEAP:\ncompare times: %d, swaps: %d\n", cnt_cmp_heap, cnt_swap_heap);
 
-    time_t t = time(0);
-    srand(t);
-    int n;
-    scanf("%d", &n);
-    double *a = (double*)malloc(n * sizeof(double));
-    rand_d(a, n);
+        if (n == 10) {
+            for (int i = 0;i < n;i++) {
+                printf("%f ", a[i]);
+            }
+            printf("\n");
+            for (int i = 0;i < n;i++) {
+                printf("%f ", b[i]);
+            }
+            printf("\n");
+        }
 
-    //shellsort(a, n);
-
-    //heapsort(a, n);
-    for (int i = 0;i < n;i++) {
-        printf("%f ", a[i]);
+        free(a);
     }
-    free(a);
     return 0;
 }
